76b316947879043af9df68e60655b4ed215ee503
Zephyr <phillip.t.mock@gmail.com>
2022-05-24 13:10:45 -0700

    appointment.js commit

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,12 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, Model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const appointmentSchema = new Schema({
-  appointmentText: {
+  _id: {
     type: String,
-    required: 'You need to make an appointment!',
-    minlength: 1,
-    maxlength: 280,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  startLocation: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  endLocation: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  date: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  bedroomNumber: {
+    type: Number,
+    required: true,
     trim: true,
   },
   appointmentAuthor: {
@@ -14,11 +47,7 @@ const appointmentSchema = new Schema({
     required: true,
     trim: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
+  
 });
 
 const Appointment = model('Appointment', appointmentSchema);
