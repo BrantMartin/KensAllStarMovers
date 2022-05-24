6d24aa6e2f3ce4937c436aefdd719fa28841f2ab
Zephyr <phillip.t.mock@gmail.com>
2022-05-24 11:10:17 -0700

    initial model changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Appointment = require('./Appointment');
 
-module.exports = { User, Thought };
+module.exports = { User, Appointment };
