2750985ee59256be3fdc9f122eb4a4caecbc7ba2
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-05-27 01:13:47 -0700

    added alert for successfully submitted appointment

--- client/src/components/AppointmentForm/index.js
+++ client/src/components/AppointmentForm/index.js
@@ -266,7 +266,7 @@ const AppointmentForm = () => {
               </div>
             </div>
 
-            <button
+            <button onClick={() => {alert("Successfully saved appointment to your profile!"); window.location.reload();}}
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
